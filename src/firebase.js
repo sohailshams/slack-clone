c3c032141b6e5c6d241d23efbbffafc2fff00cd6
sohailshams <sohailshamas@yahoo.com>
2021-02-25 12:04:12 +0100

    Added firebase configrations

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCgG5nb52TZ2o6GE_KJC7Pduhi3JPya8ng',
+  authDomain: 'slack-clone-7a183.firebaseapp.com',
+  projectId: 'slack-clone-7a183',
+  storageBucket: 'slack-clone-7a183.appspot.com',
+  messagingSenderId: '228591978802',
+  appId: '1:228591978802:web:07a3e182f8a4e58bbc2f2b',
+};
