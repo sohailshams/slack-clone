fad674c1c94f5cc3fdb3a3995594f56a99699443
sohailshams <sohailshamas@yahoo.com>
2021-03-08 19:30:26 +0100

    Added hover effect to the sidebar and changed the font weight of the title and added padding to the span

--- src/components/SidebarOption.styles.js
+++ src/components/SidebarOption.styles.js
@@ -6,6 +6,19 @@ export const SidebarOptionContainer = styled.div`
   align-items: center;
   padding-left: 2px;
   cursor: pointer;
+
+  :hover {
+    opacity: 0.9;
+    background-color: #340e36;
+  }
+
+  > h3 {
+    font-weight: 500;
+  }
+
+  > h3 > span {
+    padding: 15px;
+  }
 `;
 
 export const SidebarOptionChennel = styled.div``;
