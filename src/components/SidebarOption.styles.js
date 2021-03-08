52d0bd2b1ebf6e2b6a0c00f5eb80e04d5bf363c6
sohailshams <sohailshamas@yahoo.com>
2021-03-08 18:57:46 +0100

    Added styling to sidebar icons and titles

--- src/components/SidebarOption.styles.js
+++ src/components/SidebarOption.styles.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-export const SidebarOptionContainer = styled.div``;
+export const SidebarOptionContainer = styled.div`
+  display: flex;
+  font-size: 12px;
+  align-items: center;
+  padding-left: 2px;
+  cursor: pointer;
+`;
 
 export const SidebarOptionChennel = styled.div``;
