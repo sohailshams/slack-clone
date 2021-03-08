28cd8ac85dc384a7a148370b0516fe5452a27fba
sohailshams <sohailshamas@yahoo.com>
2021-03-08 18:51:34 +0100

    Created SidebarOptionContainer styled component and conditionally  rendered the title

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { SidebarOptionContainer } from './SidebarOption.styles';
+import {
+  SidebarOptionChennel,
+  SidebarOptionContainer,
+} from './SidebarOption.styles';
 
 function SidebarOption({ Icon, title }) {
   return (
     <SidebarOptionContainer>
       {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <SidebarOptionChennel>
+          <span>#</span> {title}
+        </SidebarOptionChennel>
+      )}
     </SidebarOptionContainer>
   );
 }
