0b6f77c0f30a6af5ad2faf42760353349da00036
sohailshams <sohailshamas@yahoo.com>
2021-03-09 09:58:58 +0100

    Added onClick function - declared selectChennel function and addChennel function to get chennel name from user and add it in the db

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { db } from '../firebase';
 import {
   SidebarOptionChennel,
   SidebarOptionContainer,
 } from './SidebarOption.styles';
 
 function SidebarOption({ Icon, title, addChannelOption }) {
+  const addChennel = () => {
+    const chennelName = prompt('Please enter the chennel name');
+
+    // If statement to protect against if user try to add empty string
+    if (chennelName) {
+      db.collection('rooms').add({
+        name: chennelName,
+      });
+    }
+  };
+  const selectChennel = () => {};
   return (
-    <SidebarOptionContainer>
+    <SidebarOptionContainer
+      onClick={addChannelOption ? addChennel : selectChennel}
+    >
       {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
       {Icon ? (
         <h3>{title}</h3>
