9dd4d818b4de6c4c0e9c83111921d4cf35baea5e
sohailshams <sohailshamas@yahoo.com>
2021-03-09 09:33:00 +0100

    Installed firebase and added settings to firebase.js to access the db, auth and provider

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyCgG5nb52TZ2o6GE_KJC7Pduhi3JPya8ng',
   authDomain: 'slack-clone-7a183.firebaseapp.com',
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: '228591978802',
   appId: '1:228591978802:web:07a3e182f8a4e58bbc2f2b',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
