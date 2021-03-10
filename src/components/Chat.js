a3f0bd1c2005d6f5d06ee715bb40b57c947991b6
sohailshams <sohailshamas@yahoo.com>
2021-03-10 23:09:25 +0100

    Pulled the roomDeatils and roomMessages from the db

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -11,9 +11,23 @@ import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import Chatinput from './Chatinput';
 import { selectRoomId } from '../features/appSlice';
 import { useSelector } from 'react-redux';
+import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
+import { db } from '../firebase';
 
 function Chat() {
   const roomId = useSelector(selectRoomId);
+  const [roomDetails] = useDocument(
+    roomId && db.collection('rooms').doc(roomId)
+  );
+  const [roomMessages] = useCollection(
+    roomId &&
+      db
+        .collection('rooms')
+        .doc(roomId)
+        .collection('messages')
+        .orderBy('timestamp', 'asc')
+  );
+
   return (
     <ChatContainer>
       <>
