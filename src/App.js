07975e193279df86e5558de0b6fde7f5e1449f4b
sohailshams <sohailshamas@yahoo.com>
2021-03-11 13:48:34 +0100

    Imported useAuthState, auth, Login and added a condition to render Login component

--- src/App.js
+++ src/App.js
@@ -5,22 +5,30 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import styled from 'styled-components';
 import Chat from './components/Chat';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase';
+import Login from './components/Login';
 
 function App() {
+  const [user, loading] = useAuthState(auth);
   return (
     <div className="app">
       <Router>
-        <>
-          <Header />
-          <AppBody>
-            <Sidebar />
-            <Switch>
-              <Route path="/" exact>
-                <Chat />
-              </Route>
-            </Switch>
-          </AppBody>
-        </>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <AppBody>
+              <Sidebar />
+              <Switch>
+                <Route path="/" exact>
+                  <Chat />
+                </Route>
+              </Switch>
+            </AppBody>
+          </>
+        )}
       </Router>
     </div>
   );
