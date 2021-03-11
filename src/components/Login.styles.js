42fc55b3588ce66a2a224872b832ba93e7ca2fd7
sohailshams <sohailshamas@yahoo.com>
2021-03-11 13:57:28 +0100

    Added styling to LoginInnerContainer styled component for the background of slack logo

--- src/components/Login.styles.js
+++ src/components/Login.styles.js
@@ -8,6 +8,12 @@ export const LoginContainer = styled.div`
 `;
 
 export const LoginInnerContainer = styled.div`
+  padding: 100px;
+  text-align: center;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+
   > img {
     object-fit: contain;
     height: 100px;
