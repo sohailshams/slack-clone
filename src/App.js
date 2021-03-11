f757980fbea56e21d303bdae9c86f5032d784fde
sohailshams <sohailshamas@yahoo.com>
2021-03-11 21:46:51 +0100

    Installed react-spinkit - created AppLoading and AppLoadingContents styled components, added spinner and slack logo and styled it

--- src/App.js
+++ src/App.js
@@ -8,9 +8,25 @@ import Chat from './components/Chat';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase';
 import Login from './components/Login';
+import Spinner from 'react-spinkit';
 
 function App() {
   const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <Apploading>
+        <AppLoadingContents>
+          <img
+            src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+            alt="slack image"
+          />
+          <Spinner name="folding-cube" color="purple" fadeIn="none" />
+        </AppLoadingContents>
+      </Apploading>
+    );
+  }
+
   return (
     <div className="app">
       <Router>
@@ -39,4 +55,25 @@ export default App;
 const AppBody = styled.div`
   display: flex;
   height: 100vh;
+`;
+
+const Apploading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    height: 100px;
+    padding: 20px;
+  }
 `;
