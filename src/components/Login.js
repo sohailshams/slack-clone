6bb23d290dc454022553cafe8c3c56fa0d00874a
sohailshams <sohailshamas@yahoo.com>
2021-03-11 13:20:57 +0100

    Created Login component, LoginContainer and LoginInnerContainer styled components and rendered slack image

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { LoginContainer, LoginInnerContainer } from './Login.styles';
+
+function Login() {
+  return (
+    <LoginContainer>
+      <LoginInnerContainer>
+        <img
+          src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+          alt=""
+        />
+      </LoginInnerContainer>
+    </LoginContainer>
+  );
+}
+
+export default Login;
---
+++ src/components/Login.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const LoginContainer = styled.div`
+  background-color: #f8f8f8;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+`;
+
+export const LoginInnerContainer = styled.div`
+  > img {
+    object-fit: contain;
+    height: 100px;
+    margin-bottom: 40px;
+  }
+`;
